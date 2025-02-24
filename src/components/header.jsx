51120c5552725a645fd75d165c276ffecfea6f1b
uddeshyac9 <uddeshyac9@gmail.com>
2025-02-24 18:53:34 +0530

    color-font-size-updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,11 +8,11 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h2 style={{color:'#FFA500',  }}>
+                <h2 style={{color:'#f23535',  }}>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h2>
-                <p style={{   color: "#2e6ea5", fontWeight:'bold'}}>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p style={{   color: "#FFA500", fontWeight:'bold'}}>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
   href="https://wa.me/7906767266?text=Hello! I would like to learn more about your services."
   className="btn btn-custom btn-lg page-scroll"
